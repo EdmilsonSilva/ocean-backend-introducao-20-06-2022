d1954c1f1255e6dc35d19c0b4648dff87d5accd9
Edmilson Silva <ed1000sons@gmail.com>
2022-06-20 16:32:58 +0000

    feat: add endpoints

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
+const port = 3000;
+const heroes = ["Mulher Maravilha", "Capitã Marvel", "Vespa", "Ms Marvel", "Valkyrie", "Homem de Ferro"];
 
 app.get("/", (_req, res) => {
     res.status(200).send("Hello World");
 })
 
-const port = 3000;
+// # Endpoint Read All - [GET] /heroes 
+app.get("/heroes", (_req, res) => {
+    res.status(200).json(heroes);
+});
+
+// # Endpoint Read One - [GET] /heroes/:id
+app.get("/heroes/:id", (req, res) => {
+    if(heroes[req.params.id - 1]) {
+        return res.status(200).json(heroes[req.params.id - 1]);
+    }
+    res.sendStatus(404);
+})
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 })
